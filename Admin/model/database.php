5bfdaef006daf9a72d2a5ff2bc3ca976f42ae5c6
davsgladden <98273428+davsgladden@users.noreply.github.com>
2022-03-23 17:07:35 -0400

    Update database.php

--- Admin/model/database.php
+++ Admin/model/database.php
@@ -1,10 +1,10 @@
 <?php
     $dsn = 'mysql:host=pk1l4ihepirw9fob.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=fltp7dq2s3ninu3t';
     $username = 'b1yigybqycgbh56h';
-    //$password = 'x98fnb2jyqm2y8la';
+    $password = 'x98fnb2jyqm2y8la';
 
     try {
-        $db = new PDO($dsn, $username);//, $password);
+        $db = new PDO($dsn, $username, $password);
     } catch (PDOException $e) {
         $error_message = $e->getMessage();
         include('view/error.php');
